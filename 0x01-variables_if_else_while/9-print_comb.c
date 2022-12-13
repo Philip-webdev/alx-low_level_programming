a0777fe26e6bd20b9156a25d97b34945ebaec172
Philip-webdev <philisobank@gmail.com>
2022-12-12 21:15:39 -0800

    var 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,18 +6,16 @@
 */
 int main(void)
 {
-	int n;
+	int n = 0;
 
-
-	for  (n = 48; n < 58; n++)
-
-		putchar(n);
-	if (n != 57)
+	while (n < 10)
 	{
-		putchar(' ');
-		putchar(',');
+		putchar(48 + n);
+		if (n != 9)
+		{
+			putchar(' ');
+			putchar(',');
+		}
+		n++;
 	}
-	putchar('\n');
-
-	return (0);
 }
