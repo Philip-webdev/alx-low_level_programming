34ede6f399a9defe447f92d55ffc4af5ecd21453
Philip-webdev <philisobank@gmail.com>
2022-12-19 15:25:06 -0800

    pointer 0

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts - prints a string followed by a new line to stdout
+ * @str: the string
+ * Return: 0
+*/
+
+void _puts(char *str)
+{
+	int a = 0;
+
+	while (str[a] != '\0')
+	{
+		_putschar(*str++);
+	}
+	_putchar('\n');
+}
