943560f608f4dafc3356723b9f95f3b2284b4208
Philip-webdev <philisobank@gmail.com>
2022-12-19 16:04:16 -0800

    pointer 0

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,7 +2,7 @@
 /**
  * _puts - prints a string followed by a new line to stdout
  * @str: the string
- * Return: 0
+ * Return: the length of the string
 */
 
 void _puts(char *str)
@@ -11,7 +11,8 @@ void _puts(char *str)
 
 	while (str[a] != '\0')
 	{
-		_putschar(*str++);
+		_putchar(str[a]);
+		a++;
 	}
 	_putchar('\n');
 }
