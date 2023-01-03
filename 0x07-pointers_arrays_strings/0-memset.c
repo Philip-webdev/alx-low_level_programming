659bdb1d6cd351f967bc0f8131890e372fb47552
Philip-webdev <philisobank@gmail.com>
2023-01-03 01:12:54 -0800

    more pointers

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,16 +1,20 @@
 #include "main.h"
 /**
- * _memset() - writes a function that fils memory with a constant byte
- * @n - byte
- * @s - pointer
- * @b - constant byte
+ * _memset - writes a function that fils memory with a constant byte
+ * @n: byte
+ * @s: pointer
+ * @b: constant byte
  * Return: Nothing
 */
 char *_memset(char *s, char b, unsigned int n)
-	unsigned int n;
-	int *s;
-	char buffer[n] = {0x00};
-	*s = buffer[n];
+{
+	unsigned int i = 0;
 
-	const byte b[10];
-	memset(buffer[n], sizeof(b));
+	while (i < n)
+	{
+		*(s + i) = b;
+		i++;
+	}
+	return (s);
+}
+
